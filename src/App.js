243db0cd409808342be684ba5278053bb4882a48
rcmolly305 <rcmolly305@protonmail.com>
2021-01-29 11:46:10 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Esto es un ejemplo de página sin editar.
         </p>
+        <Header />
         <a
           className="App-link"
           href="https://reactjs.org"
